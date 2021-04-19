5988b19f7a89206f33afd1d5bbdbd1b7747e44f0
carlos <joscaralvsan2003@gmail.com>
2021-04-19 13:57:43 +0200

    añadido

---
+++ chuleta/my-app/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import FirstComponents from './components/FirstComponents';
+
+function App() {
+  return (
+   <FirstComponents />
+  );
+}
+
+export default App;
---
+++ chuleta/my-app/src/components/FirstComponents.js
@@ -0,0 +1,10 @@
+
+export default function FirstComponents() {
+    return (
+        <div>
+            <h3>titulo</h3>
+            <small>16 de Agosto de 2025</small>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptas!</p>
+        </div>
+    )
+}
