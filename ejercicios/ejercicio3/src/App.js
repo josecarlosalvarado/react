b95f90a22e5ca4e871a992d9b863b0dadc5cf84c
carlos <joscaralvsan2003@gmail.com>
2021-04-22 14:03:11 +0200

    añadido

--- ejercicios/ejercicio3/src/App.js
+++ ejercicios/ejercicio3/src/App.js
@@ -1,29 +1,25 @@
 import './App.css';
 import Agenda from "./componentes/Agenda";
-// import Formulario from "./componentes/Formulario";
+import Formulario from "./componentes/Formulario";
+import { useState } from "react";
 
 
 function App() {
 
-const contactos = {
-  contacto1: [
-    {nombre: "carlos", apellidos: "alvarado", direccion:"calle alvarez", ciudad:"Malaga", codigoPostal:"20465", telefono:"666666666"}
-  ],
-  contacto2: [
-    {nombre: "Jose", apellidos: "gamez", direccion:"calle molina", ciudad:"madrid", codigoPostal:"45678", telefono:"777777777"}
-  ],
-  contacto3: [
+const initialContactState = [
+    {nombre: "carlos", apellidos: "alvarado", direccion:"calle alvarez", ciudad:"Malaga", codigoPostal:"20465", telefono:"666666666"},
+    {nombre: "Jose", apellidos: "gamez", direccion:"calle molina", ciudad:"madrid", codigoPostal:"45678", telefono:"777777777"},
     {nombre: "carlota", apellidos: "martinez", direccion:"calle larios", ciudad:"Malaga", codigoPostal:"87695", telefono:"888888888"}
-  ]
-}
+]
+
+const [contactos, setContactos] = useState(initialContactState)
 
   return (
-    <div className="App">
-        <Agenda contacto={contactos.contacto1} titulo={"contacto 1"}/>
-        <Agenda contacto={contactos.contacto2} titulo={"contacto 2"}/>
-        <Agenda contacto={contactos.contacto3} titulo={"contacto 3"}/>
-
-        {/* <Formulario /> */}
+    <div className="container">
+      <h2 className="my-4">Agenda</h2>
+        <Agenda contacto={contactos} />
+        <h2 className="my-4">Formulario</h2>
+        <Formulario setContactos={setContactos} />
     </div>
   );
 }
--- ejercicios/ejercicio3/src/componentes/Agenda.js
+++ ejercicios/ejercicio3/src/componentes/Agenda.js
@@ -1,34 +1,48 @@
-export default function Agenda(props) {
 
-    const style = {
-        color: 'white',
-        backgroundColor: 'blue',
-    }
 
-    return(
-        <>
-            
-                {
-                    props.contacto.map(contacto => {
-                        return <ul className="list-group m-auto w-50">
-                                    <h2 style={style}>{props.titulo}</h2>
-                                    <li className="list-group-item ">{contacto.nombre}</li>
-                                    <li className="list-group-item">{contacto.apellidos}</li>
-                                    <li className="list-group-item">{contacto.direccion}</li>
-                                    <li className="list-group-item">{contacto.ciudad}</li>
-                                    <li className="list-group-item">{contacto.codigoPostal}</li>
-                                    <li className="list-group-item">{contacto.telefono}</li>
-                               </ul>
-                        
-                    })
-                }
-        </>
+
+
+export default function Agenda({ contacto }) {
+
+
+    return (
+        <div className="row">
+            {contacto.map((contacto,index) => {
+                return (
+                        <ul className="list-group mb-3 col-6" key={contacto.telefono}>
+                            <li className="list-group-item active">contacto {index}</li>
+                            <li className="list-group-item">{contacto.nombre}</li>
+                            <li className="list-group-item">{contacto.apellidos}</li>
+                            <li className="list-group-item">{contacto.telefono}</li>
+                            <li className="list-group-item">{contacto.direccion}, {contacto.codigoPostal}, {contacto.ciudad}</li>
+                            <li className="list-group-item"><button className=" btn btn-warning">Eliminar</button></li>
+                        </ul>
+                )
+            })}
+        </div>
     )
+
 }
 
 
+// return (
+//     <>
 
+//         {
+//             props.contacto.map(contacto => {
+//                 return <ul className="list-group m-auto w-50">
+//                     <h2 style={style}>{props.titulo}</h2>
+//                     <li className="list-group-item ">{contacto.nombre}</li>
+//                     <li className="list-group-item">{contacto.apellidos}</li>
+//                     <li className="list-group-item">{contacto.direccion}</li>
+//                     <li className="list-group-item">{contacto.ciudad}</li>
+//                     <li className="list-group-item">{contacto.codigoPostal}</li>
+//                     <li className="list-group-item">{contacto.telefono}</li>
+//                     <li><button className="btn btn-warning list-group-item">Eliminar</button></li>
+//                 </ul>
 
-// <li className="list-group-item m-auto">{contacto.nombre} {contacto.apellidos} {contacto.direccion} {contacto.ciudad}
-//                                 {contacto.codigopostal} {contacto.telefono}
-//                                 </li> 
+//             })
+//         }
+//     </>
+// )
+
