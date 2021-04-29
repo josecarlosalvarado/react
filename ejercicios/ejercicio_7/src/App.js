f97a649ca9cbe1209e78a3dd4f6b73f0f6de527a
carlos <joscaralvsan2003@gmail.com>
2021-04-29 13:49:38 +0200

    añadido

--- ejercicios/ejercicio_7/src/App.js
+++ ejercicios/ejercicio_7/src/App.js
@@ -7,20 +7,22 @@ import './App.css';
 
 function App() {
 
-const [Movie, setListaArray] = useState([]);
+const [Movies, setListaArray] = useState([]);
+
+
 
 const URL_UPCOMING = "https://api.themoviedb.org/3/movie/upcoming?api_key=fc514dfc2ab8931cf6434c2853ad7cfa";
 
 useEffect(() => {
 fetch(URL_UPCOMING)
   .then(response => response.json())
-  .then(data => setListaArray(data));
+  .then(data => setListaArray(data.results.slice(0,5)));
 }, []);
 
 
   return (
     <div className="App">
-      <Lista Movie={Movie}/>
+      <Lista Movies={Movies}/>
     </div>
   );
 }
--- ejercicios/ejercicio_7/src/componentes/Lista.js
+++ ejercicios/ejercicio_7/src/componentes/Lista.js
@@ -1,13 +1,25 @@
 
 
-export default function lista( { Movie } ) {
+export default function lista( { Movies } ) {
+
+    const IMG_URL = "https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-ES" 
+
     return (
-            <ul>
-                {Movie.map(pelicula => {
+            <div className="">
+                 {Movies.map(pelicula => {
                     return (
-                        <li>{pelicula.results.title}</li>
+                       <div className="card container mb-5 mt-5">
+                            <img src={`https://api.themoviedb.org/3/movie/${pelicula?.poster_path}/images?api_key=<<api_key>>&language=en-ES`} alt=""/>
+                            <div className="card-body">
+                                <h5 className="card-title">{pelicula?.title}</h5>
+                                <p className="card-text">{pelicula?.overview}</p>
+                                <button className="btn btn-primary">Informacion</button>
+                            </div>
+                       </div>
                     )
                 })}
-            </ul>
+            </div>
+               
+           
     )
 }
